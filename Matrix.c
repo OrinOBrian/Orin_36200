60863007e122ace8f4eec6074c9e5c59a738c451
OrinOBrian <oobrian@purdue.edu>
2025-02-19 22:28:43 -0500

    final project 1

--- Matrix.c
+++ Matrix.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct LMat {
     struct LMat *right, *left, *down, *up; 
@@ -13,7 +14,9 @@ void printM(LMat* head);
 void stprintM(LMat* head, int s);
 void clprintM(LMat* head, int s);
 void fprintM(LMat* head, FILE *fp);
+void fgetsM(LMat *root, const char *filename);
 void freeM(LMat* head);
+
 
 int main() {
     LMat *root = (LMat *)malloc(sizeof(LMat));
@@ -22,26 +25,73 @@ int main() {
     root->left = NULL;
     root->down = NULL;
     root->up = NULL;
-    
-    
-    
-    packM(root,3,1);
-    packM(root,4,9);
-    packM(root,5,9);
-    packM(root,5,200);
-    // print matrix
-    printM(root);
-    // print to file
-    FILE *fp = fopen("matrix.txt", "w");
-    if (fp != NULL) {
-        fprintM(root, fp);
-        fclose(fp);
+    char c ='0';
+    while(c!='q'){
+        printf("\n_________________________\n");
+        printf("| Register Student: m\n| Print Students Classes:s\n| Print Classes Roster:c\n| Print Matrix:p\n| Read from File:r\n| Save to file:f\n| Quit:q");
+        printf("\n|________________________\n");
+        printf("Enter a Command... ");
+        int catch = scanf(" %c",&c);
+        if (catch != 1) {
+            printf("Error reading character.\n");
+            break; 
+        }
+
+        switch(c){
+            case 'm':{
+                //set or create value
+                int st;
+                int cl;
+                printf("Enter Student Number... ");
+                catch = scanf("%d",&st);
+                printf("Enter Class Number for Registration... ");
+                int throw = scanf("%d",&cl);
+                if((catch==1)&&(throw==1))
+                    packM(root,st,cl);
+                else printf("Not a Valid Input\n");
+                break;
+            }
+            case 'p':
+                // print matrix
+                printM(root);
+                break;
+            case 'f':{
+                // print to file
+                FILE *fp = fopen("matrix.txt", "w");
+                if (fp != NULL) {
+                    fprintM(root, fp);
+                    fclose(fp);
+                }
+                break;
+            }
+            case 's':{
+                int st;
+                //print a students class list
+                printf("Enter Student Number... ");
+                int throw = scanf("%d",&st);
+                if(throw==1)
+                    stprintM(root, st);
+                else printf("Not a Valid Input\n");
+                break;
+            }
+            case 'c':{
+                //print a classes student list
+                int cl;
+                printf("Enter Class Number... ");
+                int throw = scanf("%d",&cl);
+                if(throw==1)
+                    clprintM(root, cl);
+                else printf("Not a Valid Input\n");
+                break;
+            }
+            case 'r':{
+                fgetsM(root, "input.txt");
+                break;
+            }
+            default:
+                break;
+        }
     }
-    
-
-    stprintM(root, 4);
-    clprintM(root, 9);
-
     // free memory
     freeM(root);
     return 0;
@@ -145,12 +195,13 @@ void packM(LMat* head, int std, int cls) {
 }
 
 // print all matrix
+// test function to verify the matrix
 void printM(LMat* head) {
     LMat* rowStart = head;
     while(rowStart != NULL) {
         LMat* current = rowStart;
         while(current != NULL) {
-            printf( "%d ", current->s);
+            printf( "%d\t", current->s);
             current = current->right;
         }
         printf("\n");
@@ -162,15 +213,16 @@ void printM(LMat* head) {
 void stprintM(LMat* head, int s) {
     LMat* rowStart = head;
     LMat* current = head;
-    while (current != NULL) {
+    while (current->right != NULL) {
         if (current->s == s) {
-            printf("StID %d: ", current->s);
+            printf("\nStudent ID %d:", current->s);
             current=current->down;
             rowStart = rowStart->down;
             break;
         }
         current = current->right;
     }
+    printf("\nClasses: ");
     while (rowStart != NULL) {
         if(current->s > 0)
             printf("%d ",rowStart->s);
@@ -178,21 +230,23 @@ void stprintM(LMat* head, int s) {
         rowStart = rowStart->down;
     }
     printf("\n");
+    return;
 }
 
 // Search for cln s and if taken print the student numbers
 void clprintM(LMat* head, int s) {
     LMat* colStart = head;
     LMat* current = head;
-    while (current != NULL) {
+    while (current->down != NULL) {
         if (current->s == s) {
-            printf("ClassID %d: ", current->s);
+            printf("\nClass ID %d:", current->s);
             current=current->right;
             colStart = colStart->right;
             break;
         }
         current = current->down;
     }
+    printf("\nStudents: ");
     while (colStart != NULL) {
         if(current->s > 0)
             printf("%d ",colStart->s);
@@ -200,20 +254,58 @@ void clprintM(LMat* head, int s) {
         colStart = colStart->right;
     }
     printf("\n");
+    return;
 }
 
 // Print matrix to file
 void fprintM(LMat* head, FILE *fp) {
     LMat* rowStart = head;
+    LMat* current = rowStart;
+    current=current->right;
+    rowStart = rowStart->down;
     while(rowStart != NULL) {
-        LMat* current = rowStart;
-        while(current != NULL) {
-            fprintf(fp, "%d ", current->s);
-            current = current->right;
+        fprintf(fp,"%d ", current->s);
+        current=current->down;
+        while (rowStart != NULL) {
+            if(current->s > 0)
+                fprintf(fp,"%d ",rowStart->s);
+            current=current->down;
+            rowStart = rowStart->down;
         }
         fprintf(fp, "\n");
         rowStart = rowStart->down;
     }
+}
+
+//Reads st,cl,cl,cl from a file 
+void fgetsM(LMat *root, const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp)) {
+        // Remove newline if present
+        line[strcspn(line, "\n")] = 0;
+        
+        char *token = strtok(line, ",");
+        if (token == NULL) continue;
+        
+        // First token is the student number
+        int student = atoi(token);
+        
+        // Remaining tokens are class numbers
+        while ((token = strtok(NULL, ",")) != NULL) {
+            int class = atoi(token);
+            if (student > 0 && class > 0) {
+                packM(root, student, class);
+            }
+        }
+    }
+    
+    fclose(fp);
 }
 
 // Free matrix memory
